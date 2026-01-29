2c2f3e1465f0dbb5ebfaaf7065120ee052e9bfcb
Gabriel <gabriel.capila20@gmail.com>
2026-01-29 18:03:59 -0300

    teste url

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* Configuração */
-const API_BASE_URL = "https://localhost:5007"; // ou http://localhost:5000
+const API_BASE_URL = "/ticketmaster/api"; // ou http://localhost:5000
 
 let lastLoadedReport = null;
 let isBusy = false;
